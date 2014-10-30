6637b7a65148d55b06c0defb49b3005f217ac95e
Martin Liu <hflh1989@gmail.com>
2014-10-30 23:37:47 +0800

    init

---
+++ src/main/java/com/martinliu/App.java
@@ -0,0 +1,17 @@
+package com.martinliu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public int te(){
+        return 1;
+    }
+
+    public void static main(String[] args){
+
+    }
+
+}

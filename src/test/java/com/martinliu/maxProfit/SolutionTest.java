6e5e8cd822684c69a2d2b34780979d5ac3c4f5c1
Martin Liu <hflh1989@gmail.com>
2014-11-18 23:41:02 +0800

    add max profit solution

---
+++ src/test/java/com/martinliu/maxProfit/SolutionTest.java
@@ -0,0 +1,36 @@
+package com.martinliu.maxprofit;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.*;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Unit test for Solution.
+ */
+public class SolutionTest {
+    @Test
+    public void test()
+    {
+        Solution solution = new Solution();
+
+        int[] p = {};
+        assertTrue(solution.maxProfit(p) == 0);
+
+        p = new int[]{1, 2};
+        assertTrue(solution.maxProfit(p) == 1);
+
+        p = new int[]{1, 2, 3};
+        assertTrue(solution.maxProfit(p) == 2);
+
+        p = new int[]{2, 3, 1};
+        assertTrue(solution.maxProfit(p) == 1);
+
+        p = new int[]{2, 3, 1, 2};
+        assertTrue(solution.maxProfit(p) == 2);
+    }
+}

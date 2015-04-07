ca25246cd85910d64d7d45e7878e34bcdfe9fc4a
Martin Liu <hflh1989@gmail.com>
2015-04-07 23:08:24 +0800

    finished regular-expression-matching

--- src/main/java/com/martinliu/regular-expression-matching/Solution.java
+++ src/main/java/com/martinliu/regular-expression-matching/Solution.java
@@ -30,76 +30,58 @@ public class Solution
         }
         // terminal of recursion
         else if ("".equals(p)){
-            // when terminal, if s equals "", means match
             return "".equals(s);
+        } else if ("".equals(s) && !p.endsWith("*")){
+            return false;
         } else {
-            // simplify p
-            p = convertP(p);
-            String firstP = "";
-            String restP = "";
-            // prepare firstP and restP
-            if (p.length() <= 1 ){
-                firstP = p;
-            } else if (p.charAt(1) == '*'){
-                firstP = p.substring(0, 2);
-                restP = p.substring(2, p.length());
+            if (p.endsWith("*")) {
+                return starMatch(s, p);
+            } else if (p.endsWith(".") || equalsLast(s, p)) {
+                return isMatch(removeLast(s, 1), removeLast(p, 1));
             } else {
-                firstP = p.substring(0, 1);
-                restP = p.substring(1, p.length());
-            }
-
-            // try to match sub string
-            String firstS = matchString(s, firstP);
-
-            String restS = null;
-            if (null == firstS){
-                // match fail
                 return false;
-            } else if ("".equals(firstS)){
-                // match ""
-                restS = s;
-            } else {
-                restS = s.substring(firstS.length(), s.length());
             }
-
-            return isMatch(restS, restP);
         }
     }
 
-    // simplify p
-    private String convertP(String p){
-        int index = 0;
-        int from = 0;
-        while ((index = p.indexOf('*', from)) > -1){
-            char c = p.charAt(index - 1);
-
+    private String removeLast(String s, int n) {
+        if (s == null || n > s.length()){
+            return s;
         }
-        return p;
+        String ret = s.substring(0, s.length() - n);
+        if (ret == null){
+            ret = "";
+        }
+        return ret;
     }
 
-    // match sub String of s, match fail return null, no match return ""
-    private String matchString(String s, String p){
-        if (null != s && null != p){
-            if (p.length() == 1){
-                if (p.charAt(0) == '.' || s.indexOf(p) == 0){
-                    if (s.length() > 0){
-                        return s.substring(0, 1);
-                    }
-                }
-            } else {
-                // `p.length() == 2` => `x*` style
-                if (".*".equals(p)){
-                    return s;
-                } else {
-                    char first = p.charAt(0);
-                    int i = 0;
-                    while (i < s.length() && first == s.charAt(i)){
-                        ++i;
-                    }
-                    return s.substring(0, i);
-                }
+    private boolean equalsLast(String s1, String s2) {
+        char c1 = s1.charAt(s1.length() - 1);
+        char c2 = s2.charAt(s2.length() - 1);
+        return c1 == c2;
+    }
+
+    private boolean matchLast(String s, char c){
+        if (s == null || "".equals(s)){
+            return false;
+        }
+        return c == '.' || s.charAt(s.length() - 1) == c;
+    }
+
+    private boolean starMatch(String s, String p){
+        char pre = p.charAt(p.length() - 2);
+        p = removeLast(p, 2);
+
+        if (isMatch(s, p)){
+            return true;
+        }
+
+        while (matchLast(s, pre)) {
+            s = removeLast(s, 1);
+            if (isMatch(s, p)){
+                return true;
             }
         }
-        return null;
+        return false;
     }
 }
